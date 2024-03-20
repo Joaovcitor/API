4cf95bbbe30cbc7e49f34d3d3caaa06c9f0dc850
*João <102931058+Joaovcitor@users.noreply.github.com>
2024-03-20 16:32:34 -0300

    Update alunoRoutes.js

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -6,8 +6,8 @@ import AlunoController from '../controllers/AlunoController';
 const router = new Router();
 
 router.get('/', AlunoController.index);
-router.post('/', loginRequired, AlunoController.store);
-router.put('/:id', loginRequired, AlunoController.update);
+router.post('/', AlunoController.store);
+router.put('/:id', AlunoController.update);
 router.get('/:id', loginRequired, AlunoController.show);
 router.delete('/:id', loginRequired, AlunoController.delete);
 
