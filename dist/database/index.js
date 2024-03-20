6dc4be286a7342cf0922895227f3a28c99230285
*João <102931058+Joaovcitor@users.noreply.github.com>
2024-03-20 18:11:33 -0300

    123

--- dist/database/index.js
+++ dist/database/index.js
@@ -1,4 +1,4 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _sequelize = require('sequelize');
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _sequelize = require('sequelize');
 var _database = require('../config/database'); var _database2 = _interopRequireDefault(_database);
 var _Aluno = require('../models/Aluno'); var _Aluno2 = _interopRequireDefault(_Aluno);
 var _User = require('../models/User'); var _User2 = _interopRequireDefault(_User);
@@ -6,7 +6,13 @@ var _Foto = require('../models/Foto'); var _Foto2 = _interopRequireDefault(_Foto
 
 const models = [_Aluno2.default, _User2.default, _Foto2.default];
 
-const conecction = new (0, _sequelize.Sequelize)(_database2.default);
+// Correção: Renomear a variável 'conecction' para 'connection'
+const connection = new (0, _sequelize.Sequelize)(_database2.default);
 
-models.forEach((model) => model.init(conecction));
-models.forEach((model) => model.associate && model.associate(conecction.models));
+// Inicializa cada modelo com a conexão
+models.forEach((model) => model.init(connection));
+
+// Configura as associações entre os modelos, se houver
+models.forEach((model) => model.associate && model.associate(connection.models));
+
+exports. default = connection;
