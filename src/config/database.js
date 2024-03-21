a39f7bb5f1979a61fba926dbf66995bbf54f3bcf
*João <102931058+Joaovcitor@users.noreply.github.com>
2024-03-21 10:05:38 -0300

    852

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,8 @@ const pg = require('pg');
 module.exports = {
   dialect: 'postgres',
   url: process.env.DATABASE_URL,
-  password: process.env.PASSWORD,
-  username: process.env.USERNAME,
+  // password: process.env.PASSWORD,
+  // username: process.env.USERNAME,
   define: {
     timestamps: true,
     underscored: true,
